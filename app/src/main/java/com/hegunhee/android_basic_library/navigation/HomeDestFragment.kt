3fb21fdb0babe525c64dbf2c841c2e5c2aa465a3
hegunhee <gunhee0072@gmail.com>
2022-05-06 14:41:36 +0900

    bundle test

---
+++ app/src/main/java/com/hegunhee/android_basic_library/navigation/HomeDestFragment.kt
@@ -0,0 +1,32 @@
+package com.hegunhee.android_basic_library.navigation
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hegunhee.android_basic_library.databinding.FragmentHomeDestBinding
+
+class HomeDestFragment : Fragment() {
+    private lateinit var binding: FragmentHomeDestBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeDestBinding.inflate(inflater)
+        return binding.root
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.apply {
+            binding.textview.text = "value is ${get("myArg")}"
+        }
+
+    }
+}
--- app/src/main/java/com/hegunhee/android_basic_library/navigation/HomeFragment.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/navigation/HomeFragment.kt
@@ -4,20 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.hegunhee.android_basic_library.R
+import com.hegunhee.android_basic_library.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container,false)
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        oneButton.setOnClickListener {
+            sendDataNavigation(1)
+        }
+        twoButton.setOnClickListener {
+            sendDataNavigation(2)
+        }
+    }
+
+    private fun sendDataNavigation(data: Int = 0) {
+        findNavController().navigate(R.id.home_to_dest, bundleOf("myArg" to data))
     }
 }
