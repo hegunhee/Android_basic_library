782f167dbdc4b3e12a8c8603b0cda97afe7f57c2
hegunhee <gunhee0072@gmail.com>
2022-06-22 23:37:43 +0900

    drawer layout 구현

--- app/src/main/java/com/hegunhee/android_basic_library/drawerLayoutTest/DrawerLayoutMainActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/drawerLayoutTest/DrawerLayoutMainActivity.kt
@@ -2,19 +2,21 @@ package com.hegunhee.android_basic_library.drawerLayoutTest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.hegunhee.android_basic_library.R
 import com.hegunhee.android_basic_library.databinding.ActivityDrawerLayoutMainBinding
 
 class DrawerLayoutMainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityDrawerLayoutMainBinding
+    private lateinit var toolBar : Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDrawerLayoutMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        toolBar = binding.toolBar
+        setSupportActionBar(toolBar)
         setNavigation()
     }
     private fun setNavigation() = with(binding){
