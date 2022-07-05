e39110d4c85cd0473744b6510f3e9b50ae4aae6b
hegunhee <gunhee0072@gmail.com>
2022-07-05 21:23:10 +0900

    add Chip Test Activity

---
+++ app/src/main/java/com/hegunhee/android_basic_library/ChipTest/ChipTestActivity.kt
@@ -0,0 +1,12 @@
+package com.hegunhee.android_basic_library.ChipTest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hegunhee.android_basic_library.R
+
+class ChipTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chip_test)
+    }
+}
