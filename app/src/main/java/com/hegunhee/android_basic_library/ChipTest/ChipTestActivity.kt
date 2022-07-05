53a996de943590d1e4c4d2dd95643ea340bdf588
hegunhee <gunhee0072@gmail.com>
2022-07-05 21:31:40 +0900

    test Chip Style and ClickListener

--- app/src/main/java/com/hegunhee/android_basic_library/ChipTest/ChipTestActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/ChipTest/ChipTestActivity.kt
@@ -2,11 +2,25 @@ package com.hegunhee.android_basic_library.ChipTest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.hegunhee.android_basic_library.R
+import com.hegunhee.android_basic_library.databinding.ActivityChipTestBinding
 
 class ChipTestActivity : AppCompatActivity() {
+
+    private val binding : ActivityChipTestBinding by lazy {ActivityChipTestBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chip_test)
+        setContentView(binding.root)
+        initListener()
+    }
+
+    private fun initListener() = with(binding){
+//        singleChip.setOnClickListener {
+//            Toast.makeText(this@ChipTestActivity, "chip click", Toast.LENGTH_SHORT).show()
+//        }
+        singleChip.setOnCloseIconClickListener {
+            Toast.makeText(this@ChipTestActivity, "뭐지", Toast.LENGTH_SHORT).show()
+        }
     }
 }
