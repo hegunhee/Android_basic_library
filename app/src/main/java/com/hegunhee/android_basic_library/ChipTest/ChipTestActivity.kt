a326d4882c054db5841afb9a5aef666a55374872
hegunhee <gunhee0072@gmail.com>
2022-07-06 11:29:18 +0900

    chip 이름 찾는법

--- app/src/main/java/com/hegunhee/android_basic_library/ChipTest/ChipTestActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/ChipTest/ChipTestActivity.kt
@@ -3,21 +3,29 @@ package com.hegunhee.android_basic_library.ChipTest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.material.chip.Chip
 import com.hegunhee.android_basic_library.R
 import com.hegunhee.android_basic_library.databinding.ActivityChipTestBinding
 
 class ChipTestActivity : AppCompatActivity() {
 
-    private val binding : ActivityChipTestBinding by lazy {ActivityChipTestBinding.inflate(layoutInflater)}
+    private val binding: ActivityChipTestBinding by lazy {
+        ActivityChipTestBinding.inflate(
+            layoutInflater
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initListener()
     }
 
-    private fun initListener() = with(binding){
+    private fun initListener() = with(binding) {
         button.setOnClickListener {
-            Toast.makeText(this@ChipTestActivity, chipGroup.checkedChipIds.toString(), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@ChipTestActivity, chipGroup.checkedChipIds.map {
+                root.findViewById<Chip>(it).text.toString()
+            }.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 }
