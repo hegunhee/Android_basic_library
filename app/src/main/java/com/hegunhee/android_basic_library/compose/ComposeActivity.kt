860e02629ffc6b97788a4613d60156e4495ce0d3
hegunhee <gunhee0072@gmail.com>
2022-08-06 12:02:55 +0900

    compose Preview

--- app/src/main/java/com/hegunhee/android_basic_library/compose/ComposeActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/compose/ComposeActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,4 +20,10 @@ class ComposeActivity : ComponentActivity() {
 @Composable
 fun MessageCard(name : String){
     Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(name = "Android")
 }
