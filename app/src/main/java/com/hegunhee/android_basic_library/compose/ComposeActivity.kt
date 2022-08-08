d9a56179e5e8a0ec0354dc7aecf54f1a3ea45041
hegunhee <gunhee0072@gmail.com>
2022-08-08 17:17:46 +0900

    compose codelab level one

--- app/src/main/java/com/hegunhee/android_basic_library/compose/ComposeActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/compose/ComposeActivity.kt
@@ -3,32 +3,67 @@ package com.hegunhee.android_basic_library.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hegunhee.android_basic_library.R
 
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android","Jetpack Compose"))
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
 
     }
 }
 
-data class Message(val author : String, val body : String)
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(msg : Message){
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_home_24),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = msg.author, color = MaterialTheme.colors.secondaryVariant
+                , style = MaterialTheme.typography.subtitle2)
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(text = msg.body
+                    ,style = MaterialTheme.typography.subtitle2)
+            }
+        }
+
+
     }
+
 }
 
-@Preview
 @Composable
-fun PreviewMessageCard() {
-    MessageCard(msg = Message("Colleague","Hey,  take a look"))
+fun Conversation(messages : List<Message>){
+    LazyColumn{
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
 }
