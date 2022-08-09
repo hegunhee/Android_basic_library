5d0087bd2b037c7f1259c78a59c32164a38075b6
hegunhee <gunhee0072@gmail.com>
2022-08-09 14:55:11 +0900

    Button state

--- app/src/main/java/com/hegunhee/android_basic_library/compose/CodelabActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/compose/CodelabActivity.kt
@@ -3,11 +3,14 @@ package com.hegunhee.android_basic_library.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -15,15 +18,40 @@ class CodelabActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Android")
+            Surface(color = MaterialTheme.colors.primary) {
+                MyApp()
+            }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name : String){
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp),
-        color = MaterialTheme.colors.background)
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
+}
+
+@Composable
+fun Greeting(name: String) {
+    val expanded = remember{ mutableStateOf(false)}
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)){
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello, ")
+                Text(text = "$name!")
+            }
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
+
+    }
+
 }
