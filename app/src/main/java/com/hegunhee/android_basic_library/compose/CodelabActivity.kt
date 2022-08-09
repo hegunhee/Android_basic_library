6b86e89ca4a8748e28e013db4db01d8503ffd47d
hegunhee <gunhee0072@gmail.com>
2022-08-09 14:34:50 +0900

    Compose Modifier

--- app/src/main/java/com/hegunhee/android_basic_library/compose/CodelabActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/compose/CodelabActivity.kt
@@ -3,23 +3,27 @@ package com.hegunhee.android_basic_library.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class CodelabActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(color = MaterialTheme.colors.background) {
-                Greeting(name = "Android")
-            }
+            Greeting(name = "Android")
         }
     }
 }
 
 @Composable
 fun Greeting(name : String){
-    Text(text = "Hello $name")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp),
+        color = MaterialTheme.colors.background)
+    }
 }
