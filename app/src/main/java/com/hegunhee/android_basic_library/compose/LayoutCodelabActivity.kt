6836805c5d830ba9da15a071f86a5ad5296b3a05
hegunhee <gunhee0072@gmail.com>
2022-08-11 14:42:11 +0900

    compose RecyclerView LazyColumn

--- app/src/main/java/com/hegunhee/android_basic_library/compose/LayoutCodelabActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/compose/LayoutCodelabActivity.kt
@@ -3,11 +3,11 @@ package com.hegunhee.android_basic_library.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -16,14 +16,40 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 
 class LayoutCodelabActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PhotographerCard()
+            ImageList()
         }
+    }
+}
+
+@Composable
+fun ImageList(){
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(state = scrollState){
+        items(100){
+            ImageListItem(index = it)
+        }
+    }
+}
+@Composable
+fun ImageListItem(index : Int){
+    Row(verticalAlignment = Alignment.CenterVertically){
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
     }
 }
 
