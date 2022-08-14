c609bc170951febc336fe96c040a99537a377b07
hegunhee <gunhee0072@gmail.com>
2022-08-14 14:21:58 +0900

    StaggeredGrid custom layout

--- app/src/main/java/com/hegunhee/android_basic_library/compose/LayoutCodelabActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/compose/LayoutCodelabActivity.kt
@@ -3,18 +3,22 @@ package com.hegunhee.android_basic_library.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.layout.Layout
@@ -36,11 +40,11 @@ class LayoutCodelabActivity : ComponentActivity() {
 }
 
 @Composable
-fun ImageList(){
+fun ImageList() {
     val listSize = 100
     val scrollState = rememberLazyListState()
     val coroutineScope = rememberCoroutineScope()
-    Row{
+    Row {
         Button(onClick = {
             coroutineScope.launch {
                 scrollState.animateScrollToItem(0)
@@ -50,21 +54,22 @@ fun ImageList(){
         }
         Button(onClick = {
             coroutineScope.launch {
-                scrollState.animateScrollToItem(listSize-1)
+                scrollState.animateScrollToItem(listSize - 1)
             }
-        }){
+        }) {
             Text("Scroll to the end")
         }
     }
-    LazyColumn(state = scrollState){
-        items(100){
+    LazyColumn(state = scrollState) {
+        items(100) {
             ImageListItem(index = it)
         }
     }
 }
+
 @Composable
-fun ImageListItem(index : Int){
-    Row(verticalAlignment = Alignment.CenterVertically){
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
         Image(
             painter = rememberImagePainter(
                 data = "https://developer.android.com/images/brand/Android_Robot.png"
@@ -79,15 +84,17 @@ fun ImageListItem(index : Int){
 
 @Composable
 fun PhotographerCard(modifier: Modifier = Modifier) {
-    Row(modifier
-        .padding(16.dp)
-        .clickable(onClick = {})){
-       Surface(
-           modifier = Modifier.size(50.dp),
-           shape = CircleShape,
-           color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-       ) {
-       }
+    Row(
+        modifier
+            .padding(16.dp)
+            .clickable(onClick = {})
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+        }
         Column(
             modifier = Modifier
                 .padding(start = 8.dp)
@@ -104,19 +111,19 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
 @Composable
 fun MyOwnColumn(
     modifier: Modifier = Modifier,
-    content : @Composable () -> Unit
-){
+    content: @Composable () -> Unit
+) {
     Layout(
         modifier = modifier,
         content = content
-    ){ measurables , constraints ->
+    ) { measurables, constraints ->
         val placeables = measurables.map { measurable ->
             measurable.measure(constraints)
         }
         var yPosition = 0
-        layout(constraints.maxWidth,constraints.maxHeight){
-            placeables.forEach{ placeable ->
-                placeable.placeRelative(x=0,y=yPosition)
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
 
                 yPosition += placeable.height
             }
@@ -124,18 +131,9 @@ fun MyOwnColumn(
     }
 }
 
-@Composable
-fun BodyContent(modifier: Modifier = Modifier){
-    MyOwnColumn(modifier.padding(8.dp)) {
-        Text("MyOwnColumn")
-        Text("places items")
-        Text("vertically.")
-        Text("We've done it by hand!")
-    }
-}
 
 fun Modifier.firstBaselineToTop(
-    firstBaselineToTop : Dp
+    firstBaselineToTop: Dp
 ) = this.then(
     layout { measurable, constraints ->
         val placeable = measurable.measure(constraints)
@@ -145,9 +143,95 @@ fun Modifier.firstBaselineToTop(
 
         val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
         val height = placeable.height + placeableY
-        layout(placeable.width, height){
-            placeable.placeRelative(0,placeableY)
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
         }
 
     }
 )
+@Composable
+fun BodyContent(modifier: Modifier = Modifier){
+    StaggeredGrid(modifier = modifier) {
+        for(topic in topics){
+            Chip(modifier = Modifier.padding(8.dp),text = topic)
+        }
+    }
+
+}
+
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content,
+    ) { measurables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints)
+
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row],placeable.height)
+
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+
+        val height = rowHeights.sumOf { it }
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) {0}
+        for(i in 1 until rows){
+            rowY[i] = rowY[i-1] + rowHeights[i-1]
+        }
+        layout(width,height){
+            val rowX = IntArray(rows){0}
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier : Modifier = Modifier,text:String){
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black,width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ){
+        Row(
+            modifier = Modifier.padding(start = 8.dp,top = 4.dp,end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Box(modifier = Modifier
+                .size(16.dp, 16.dp)
+                .background(color = MaterialTheme.colors.secondary))
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = text)
+        }
+
+    }
+}
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
