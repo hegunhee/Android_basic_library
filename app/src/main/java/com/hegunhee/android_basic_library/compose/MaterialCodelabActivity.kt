2483c4d07fcf9eaefe1449c2bf1534f8f51ebf1c
hegunhee <gunhee0072@gmail.com>
2022-08-15 15:53:54 +0900

    Theme Codelab

---
+++ app/src/main/java/com/hegunhee/android_basic_library/compose/MaterialCodelabActivity.kt
@@ -0,0 +1,13 @@
+package com.hegunhee.android_basic_library.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MaterialCodelabActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
