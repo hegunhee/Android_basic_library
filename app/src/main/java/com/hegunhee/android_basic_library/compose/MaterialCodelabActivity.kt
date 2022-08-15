7dd5a6bdfedb661e79dce82e75ed4186a73161b4
hegunhee <gunhee0072@gmail.com>
2022-08-15 15:56:18 +0900

    Material

--- app/src/main/java/com/hegunhee/android_basic_library/compose/MaterialCodelabActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/compose/MaterialCodelabActivity.kt
@@ -2,6 +2,10 @@ package com.hegunhee.android_basic_library.compose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.material.Colors
+import androidx.compose.material.Shapes
+import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
 
 class MaterialCodelabActivity : ComponentActivity() {
 
@@ -11,3 +15,16 @@ class MaterialCodelabActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun MaterialTheme(
+    colors : Colors,
+    typography: Typography,
+    shapes: Shapes,
+    content: @Composable () -> Unit
+){
+
+}
+
+@Composable
+fun JetnewsTheme(content: @Composable () -> Unit){
+}
