c88db4950b74f5e40dd554e061eb6a93a0f26b43
hegunhee <gunhee0072@gmail.com>
2022-08-16 15:57:09 +0900

    count

--- app/src/main/java/com/hegunhee/android_basic_library/compose/StateCodelabActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/compose/StateCodelabActivity.kt
@@ -3,12 +3,39 @@ package com.hegunhee.android_basic_library.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.material.MaterialTheme
 
-class StateCodelabActivity  : ComponentActivity(){
+class StateCodelabActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colors.background
+                ) {
 
+            }
         }
     }
+}
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    val count = 0
+    Text(
+        text = "You've had $count glasses.",
+        modifier = modifier.padding(16.dp)
+    )
+}
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
 }
