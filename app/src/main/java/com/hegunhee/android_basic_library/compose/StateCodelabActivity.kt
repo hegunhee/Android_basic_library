5392d9f2fac05c84b417fa1a89e9bcc6681d0342
hegunhee <gunhee0072@gmail.com>
2022-08-16 15:51:24 +0900

    state codelabActivity

---
+++ app/src/main/java/com/hegunhee/android_basic_library/compose/StateCodelabActivity.kt
@@ -0,0 +1,14 @@
+package com.hegunhee.android_basic_library.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class StateCodelabActivity  : ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
