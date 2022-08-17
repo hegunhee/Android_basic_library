a13c5d2e75a886953dab32ade3eee518b2e3df30
hegunhee <gunhee0072@gmail.com>
2022-08-17 13:53:29 +0900

    mutable state

--- app/src/main/java/com/hegunhee/android_basic_library/compose/StateCodelabActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/compose/StateCodelabActivity.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 
 class StateCodelabActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,22 +22,53 @@ class StateCodelabActivity : ComponentActivity() {
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colors.background
-                ) {
-
+            ) {
+                WellnessScreen()
             }
         }
     }
 }
 
 @Composable
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
+            text = taskName
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+
+        }
+    }
+}
+
+@Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
-        var count by remember {mutableStateOf(0)}
-        Text("You' ve had $count glasses.")
-        Button(onClick = {count++},Modifier.padding(top = 8.dp)){
-            Text("Add one")
+    Column(modifier = Modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false},
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
+            Text("You've had $count glasses.")
         }
 
+        Button(onClick = { count++ }, enabled = count < 10) {
+            Text("Add one")
+        }
     }
 }
 
