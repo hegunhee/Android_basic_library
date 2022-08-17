e0112f8d4893e92c924d4cc3604c992ad9275fad
hegunhee <gunhee0072@gmail.com>
2022-08-17 14:10:04 +0900

    stateful stateless

--- app/src/main/java/com/hegunhee/android_basic_library/compose/StateCodelabActivity.kt
+++ app/src/main/java/com/hegunhee/android_basic_library/compose/StateCodelabActivity.kt
@@ -53,6 +53,27 @@ fun WellnessTaskItem(
 }
 
 @Composable
+fun StatelessCounter(count : Int, onIncrement : () -> Unit,modifier: Modifier = Modifier){
+    Column(modifier = modifier.padding(16.dp)) {
+        if(count > 0){
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement,Modifier.padding(top = 8.dp), enabled = count < 10){
+            Text("Add one")
+        }
+
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier){
+    var waterCount by remember { mutableStateOf(0)}
+
+    var juiceCount by remember { mutableStateOf(0)}
+    StatelessCounter(count = waterCount, onIncrement = { waterCount++})
+    StatelessCounter(count = juiceCount, onIncrement = { juiceCount++ })
+}
+@Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by rememberSaveable { mutableStateOf(0) }
@@ -67,5 +88,5 @@ fun WaterCounter(modifier: Modifier = Modifier) {
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
 }
